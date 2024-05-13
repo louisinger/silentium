44a50da45a9953d801ff2d09aaf74c25f0fe67aa
João Bordalo <bordalix@users.noreply.github.com>
2024-05-13 11:10:48 +0100

    Use primary color on settings button

--- src/icons/SettingsBlack.tsx
+++ src/icons/SettingsBlack.tsx
@@ -1,6 +1,6 @@
 export default function SettingsBlackIcon() {
   return (
-    <svg viewBox='0 0 16 16' className='w-8 h-8 text-current dark:text-current' xmlns='http://www.w3.org/2000/svg'>
+    <svg viewBox='0 0 16 16' className='w-8 h-8 text-primary' xmlns='http://www.w3.org/2000/svg'>
       <path
         fill='currentcolor'
         d='M3.5 2h-1v5h1zm6.1 5H6.4L6 6.45v-1L6.4 5h3.2l.4.5v1zm-5 3H1.4L1 9.5v-1l.4-.5h3.2l.4.5v1zm3.9-8h-1v2h1zm-1 6h1v6h-1zm-4 3h-1v3h1zm7.9 0h3.19l.4-.5v-.95l-.4-.5H11.4l-.4.5v.95zm2.1-9h-1v6h1zm-1 10h1v2h-1z'
