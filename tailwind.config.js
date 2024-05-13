2c77bb6a331bacc59e0c3201c804cc154842d744
João Bordalo <bordalix@users.noreply.github.com>
2024-05-13 10:41:26 +0100

    Fix colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,13 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: 'class',
   theme: {
-    colors: {
-      primary: '#FF204E',
-      secondary: '#00224D',
-      darkgray: '#030712',
-      darklessgray: '#0A0F1A',
-    },
     extend: {
+      colors: {
+        primary: '#FF204E',
+        secondary: '#00224D',
+        darkgray: '#030712',
+        darklessgray: '#0A0F1A',
+      },
       fontSize: {
         xxs: '0.5rem',
       },
