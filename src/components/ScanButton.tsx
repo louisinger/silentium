ed4a89e54e3c90c4b4f3d661433ec4b4c26084bd
Louis <louis@vulpem.com>
2024-05-15 16:12:43 +0200

    "scan" ==> "sync"

--- src/components/ScanButton.tsx
+++ src/components/ScanButton.tsx
@@ -1,8 +1,8 @@
 import { useContext, useState } from 'react'
-import SearchIcon from '../icons/Search'
 import Button from './Button'
 import { WalletContext } from '../providers/wallet'
 import NeedsPassword from './NeedsPassword'
+import ReloadIcon from '../icons/Reload'
 
 export default function ScanButton() {
   const { wallet, reloadWallet, scanning, scanningProgress } = useContext(WalletContext)
@@ -22,9 +22,9 @@ export default function ScanButton() {
 
   return (
     <>
-      {askPassword ? <NeedsPassword title='Scan' onMnemonic={handleMnemonicUnlock} onClose={() => setAskPassword(false)} /> : null}
+      {askPassword ? <NeedsPassword title='Sync' onMnemonic={handleMnemonicUnlock} onClose={() => setAskPassword(false)} /> : null}
       {scanning ? <p className='animate-bounce' >{wallet.scannedBlockHeight[wallet.network]} ({scanningProgress}%)</p> : null}
-      <Button progress={scanningProgress} icon={<SearchIcon />} label='Scan' onClick={() => handleScan()} disabled={scanning} />
+      <Button progress={scanningProgress} icon={<ReloadIcon />} label='Sync' onClick={() => handleScan()} disabled={scanning} />
     </>
   )
 }
