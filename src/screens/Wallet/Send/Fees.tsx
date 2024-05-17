25a03fe236c5d12864796284f808681c71b32503
Louis Singer <louis@vulpem.com>
2024-05-17 10:12:40 +0200

    better error setter

--- src/screens/Wallet/Send/Fees.tsx
+++ src/screens/Wallet/Send/Fees.tsx
@@ -39,7 +39,7 @@ export default function SendFees() {
 
       setSendInfo({ ...sendInfo, address, coinSelection: selection, total: satoshis })
       setTotalNeeded(satoshis + selection.txfee)
-      if (getBalance(wallet) < totalNeeded) setError(`Insufficient funds, you just have ${prettyNumber(getBalance(wallet))} sats`)
+      if (getBalance(wallet) < satoshis + selection.txfee) setError(`Insufficient funds, you just have ${prettyNumber(getBalance(wallet))} sats`)
       else setError('')
       return
     }
