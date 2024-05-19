f22482f5f6f479eb455f59a1f6249c160e375f68
louisinger <louis@vulpem.com>
2024-05-19 20:40:52 +0200

    vm browserify

--- craco.config.js
+++ craco.config.js
@@ -33,6 +33,7 @@ module.exports = {
         path: require.resolve('path-browserify'),
         crypto: require.resolve('crypto-browserify'),
         stream: require.resolve('stream-browserify'),
+        vm: require.resolve('vm-browserify'),
         fs: false,
       }
       return webpackConfig
